f88a906ac0b606b326ee481de6d1cf81e5ccc11c
Lauren Dove <elledoveinc@gmail.com>
2019-10-20 21:31:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,10 @@ class Triangle
     if ((@length_1 <= 0) || (@length_2 <= 0) || (@length_3 <= 0)) || ((@length_1 + @length_2 <= @length_3) || (@length_2 + @length_3 <= @length_1 ) || (@length_3 + @length_1 <= @length_2))
       raise TriangleError
       
-      elsif
+      elsif @length_1 == @length_2 && @length_2 == @length_3
+      :equilateral 
+      
+      elsif (@length_2 == @length_3) || (@length_1 == @length_3)
       
     
   end
