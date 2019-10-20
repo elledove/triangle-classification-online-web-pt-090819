97aa026301878ac9acc387058daaf56c5a53def2
Lauren Dove <elledoveinc@gmail.com>
2019-10-20 21:32:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
       elsif @length_1 == @length_2 && @length_2 == @length_3
       :equilateral 
       
-      elsif (@length_2 == @length_3) || (@length_1 == @length_3)
+      elsif (@length_2 == @length_3) || (@length_1 == @length_3) || @length_2 == @length_1
       
     
   end
