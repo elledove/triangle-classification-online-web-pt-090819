be20b637f946550fc94512b0bced109ae87b71c4
Lauren Dove <elledoveinc@gmail.com>
2019-10-20 21:28:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,7 @@ class Triangle
   
   def kind 
     if ((@length_1 <= 0) || (@length_2 <= 0) || (@length_3 <= 0)) || ((@length_1 + @length_2 <= @length_3) || (@length_2 + @length_3 <= @length_1 ) || (@length_3 + @length_1 <= @length_2))
+      raise TriangleError
     
   end
   
