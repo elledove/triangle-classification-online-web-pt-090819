c4e66656a2b6747dc3eaffe9b0989f6f572c138a
Lauren Dove <elledoveinc@gmail.com>
2019-10-20 21:33:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ class Triangle
       
       elsif (@length_2 == @length_3) || (@length_1 == @length_3) || @length_2 == @length_1
       :isosceles
-      elsif (@length_1 != @length_2) && (@length_2 != @length_3)
+      elsif (@length_1 != @length_2) && (@length_2 != @length_3) && (@length_3 != @length_1)
       
     
   end
